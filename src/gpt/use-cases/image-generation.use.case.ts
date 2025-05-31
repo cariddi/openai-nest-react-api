f345988984f0d8839333a259d09d82d36ccc3eb6
Lucas Cariddi <lucas.cariddi@compozelabs.com>
2025-05-31 20:46:06 -0300

    get a generated image from a prompt

--- src/gpt/use-cases/image-generation.use.case.ts
+++ src/gpt/use-cases/image-generation.use.case.ts
@@ -10,8 +10,25 @@ export const imageGenerationUseCase = async (
   openai: OpenAI,
   options: Options,
 ) => {
-  const { prompt, originalImage, maskImage } = options;
-  console.log({ prompt, originalImage, maskImage });
+  const { prompt } = options;
 
-  return;
+  // TODO: verification of originalImage
+
+  const response = await openai.images.generate({
+    prompt,
+    model: 'dall-e-3',
+    n: 1, // number of images we want to generate
+    size: '1024x1024',
+    quality: 'standard',
+    response_format: 'url',
+  });
+
+  // TODO: save image in file system
+  console.log({ response });
+
+  return {
+    url: response.data?.[0].url,
+    localPath: '',
+    revised_prompt: response.data?.[0].revised_prompt,
+  };
 };
